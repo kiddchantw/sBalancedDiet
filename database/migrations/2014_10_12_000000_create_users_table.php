4059b0a67f2e2229680dd7489dd6f7fb72d1fd62
K88 <kiddchantw@gmail.com>
2020-09-01 23:08:37 +0800

    feat:creat users db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->rememberToken()->nullable();;
+            $table->timestamp('token_expire_time',0)->nullable();;
+            $table->softDeletes();
         });
     }
 
